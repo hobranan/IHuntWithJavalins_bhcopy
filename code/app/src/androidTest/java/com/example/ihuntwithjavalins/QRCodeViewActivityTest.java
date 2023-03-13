c4cc2186819dd65dba2de0fb8584e72217e9b0c7
TannerLH <113555978+TannerLH@users.noreply.github.com>
2023-03-12 22:42:53 -0600

    Added Testing dependencies and Started on QRCodeViewActivityTest

---
+++ code/app/src/androidTest/java/com/example/ihuntwithjavalins/QRCodeViewActivityTest.java
@@ -0,0 +1,36 @@
+package com.example.ihuntwithjavalins;
+
+import android.app.Activity;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.example.ihuntwithjavalins.QRCode.QRCodeViewActivity;
+import com.robotium.solo.Solo;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class QRCodeViewActivityTest {
+
+    private Solo solo;
+
+    @Rule
+    public ActivityTestRule rule = new ActivityTestRule(QRCodeViewActivity.class, true, true);
+
+    @Before
+    public void setUp() throws Exception{
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+    }
+
+    @Test
+    public void start() throws Exception{
+        Activity activity = rule.getActivity();
+    }
+
+    @Test
+    public void testName() {
+
+    }
+}
