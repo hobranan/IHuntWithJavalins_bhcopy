33a7c6755b6b6884af95aedf687aa1a347caed6e
TannerLH <113555978+TannerLH@users.noreply.github.com>
2023-03-13 04:17:13 -0600

    Filled QRCodeViewActivityTest

--- code/app/src/androidTest/java/com/example/ihuntwithjavalins/QRCodeViewActivityTest.java
+++ code/app/src/androidTest/java/com/example/ihuntwithjavalins/QRCodeViewActivityTest.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
+import com.example.ihuntwithjavalins.QRCode.QRCodeImageViewActivity;
+import com.example.ihuntwithjavalins.QRCode.QRCodeLibraryActivity;
 import com.example.ihuntwithjavalins.QRCode.QRCodeViewActivity;
 import com.robotium.solo.Solo;
 
@@ -30,8 +32,37 @@ public class QRCodeViewActivityTest {
     }
 
     @Test
-    public void deleteButton() {
+    public void backButton() {
         solo.assertCurrentActivity("Wrong Activity", QRCodeViewActivity.class);
-        solo.clickOnButton("Remove this Code");
+        solo.clickOnButton(R.id.go_back);
+        solo.assertCurrentActivity("Wrong Activity", QRCodeLibraryActivity.class);
     }
+
+    @Test
+    public void quickNavButton() {
+        solo.assertCurrentActivity("Wrong Activity", QRCodeViewActivity.class);
+        solo.clickOnButton(R.id.imageButton);
+        solo.assertCurrentActivity("Wrong Activity", QuickNavActivity.class);
+    }
+
+    @Test
+    public void QRImageButton() {
+        solo.assertCurrentActivity("Wrong Activity", QRCodeViewActivity.class);
+        solo.clickOnButton(R.id.image_button);
+        solo.assertCurrentActivity("Wrong Activity", QRCodeImageViewActivity.class);
+    }
+
+    @Test
+    public void deleteQRButton() {
+        solo.assertCurrentActivity("Wrong Activity", QRCodeViewActivity.class);
+        solo.clickOnButton(R.id.btn_remove_code);
+        solo.waitForText("Confirmation", 1, 2000);
+        solo.clickOnButton("NO");
+        solo.assertCurrentActivity("Wrong Activity", QRCodeViewActivity.class);
+        solo.clickOnButton(R.id.btn_remove_code);
+        solo.waitForText("Confirmation", 1, 2000);
+        solo.clickOnButton("YES");
+        solo.assertCurrentActivity("Wrong Activity", QRCodeLibraryActivity.class);
+    }
+
 }
