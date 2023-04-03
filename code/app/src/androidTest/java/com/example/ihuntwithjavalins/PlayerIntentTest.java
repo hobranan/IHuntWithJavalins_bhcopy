4f68522c17d57ded4adb9043b6ddb7b7e5df0e5e
TannerLH <113555978+TannerLH@users.noreply.github.com>
2023-04-02 20:17:27 -0600

    PlayerIntentTest fixes

--- code/app/src/androidTest/java/com/example/ihuntwithjavalins/PlayerIntentTest.java
+++ code/app/src/androidTest/java/com/example/ihuntwithjavalins/PlayerIntentTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertTrue;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 import android.app.Activity;
+import android.content.Context;
+import android.widget.EditText;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
@@ -11,6 +13,7 @@ import androidx.test.rule.ActivityTestRule;
 import com.example.ihuntwithjavalins.Camera.CameraScanActivity;
 import com.example.ihuntwithjavalins.Player.Player;
 import com.example.ihuntwithjavalins.Player.PlayerDB;
+import com.example.ihuntwithjavalins.Profile.ProfileActivity;
 import com.example.ihuntwithjavalins.QRCode.QRCode;
 import com.example.ihuntwithjavalins.common.DBConnection;
 import com.robotium.solo.Solo;
@@ -25,11 +28,13 @@ import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * PlayerIntentTest is a class that tests all the Player user stories (US 01.01.01 through US 01.07.01)
+ * TODO: Make it so that these tests passes without logging in prior on your machine OR that the test machine has logged in
  *
  * @version 1.0
  */
 
 public class PlayerIntentTest {
+
 
     /**
      * The Robotium variable we will be using to run tests as if we were a user
@@ -41,6 +46,7 @@ public class PlayerIntentTest {
     QRCode mockCode1;
     QRCode mockCode2;
     QRCode mockCode3;
+
 
 
     /**
@@ -76,20 +82,45 @@ public class PlayerIntentTest {
 
     /**
      * Sets up the Activity before every test
+     * creates a player with 2 QR codes and logs in as that player
+     * Creates an extra QR code for later testing
      */
     @Before
     public void setUp() throws Exception {
 
+        //InstrumentationRegistry instumentation = new InstrumentationReg
+
+
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();          //getTargetContext();
+        context.getSharedPreferences("Login", 0).edit().clear().commit();
+
+
+        //Context context = instrumentation.getTargetContext();
+        //PreferenceManager.getDefaultSharedPreferences(context).edit().clear().commit();
+
+        //context.getSharedPreferences("XXX", Context.MODE_PRIVATE).edit().clear().commit();
+        //getSharedPreferences("Login", 0).edit().clear().commit();
+
+        // mPrefs = getSharedPreferences("Login", 0);
+        // savedDeviceUser = mPrefs.getString("UsernameTag", "default_username_not_found");
+
+
+        //SharedPreferences settings
+
+        //getSharedPreferences("Login", 0).edit().clear().commit();
+
         CountDownLatch latch = new CountDownLatch(1);
+        CountDownLatch addLatch = new CountDownLatch(1);
+
 
         solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
-        mockPlayer = new Player("John Doe", "jd@ualberta.ca", "Canada", "20230101");
-        mockCode1 = new QRCode("9D211", "Mindless Dragon Testudine", "500", "picture_min.png", "53.5", "-113.5", "2023.jpg", "20230312");
-        mockCode2 = new QRCode("9D212", "Mindfull Lizard Crustation", "400", "picture_min.png", "53.5", "-113.5", "2023.jpg", "20230312");
-        mockCode3 = new QRCode("9D213", "Mindless Dragon Testudine", "100", "picture_min.png", "53.5", "-113.5", "2023.jpg", "20230312");
+        mockPlayer = new Player("John Doe", "jd@ualberta.ca", "Edmonton", "20230101");
+        mockCode1 = new QRCode("9D211", "Charlie Dragon Testudine", "500", "picture_min.png", "53.5", "-113.5", "2023.jpg", "20230312");
+        mockCode2 = new QRCode("9D212", "Bravo Lizard Crustation", "400", "picture_min.png", "53.5", "-113.5", "2023.jpg", "20230312");
+        mockCode3 = new QRCode("9D213", "Alpha Dragon Testudine", "100", "picture_min.png", "53.5", "-113.5", "2023.jpg", "20230312");
 
         DBConnection connection = new DBConnection(rule.getActivity());
-        connection.setUsername(rule.getActivity(), "John Doe");
+        //connection.setUsername(rule.getActivity(), "John Doe");
         playerDB = new PlayerDB(connection);
 
         AtomicReference<Boolean> successAtomic = new AtomicReference<>();
@@ -99,6 +130,30 @@ public class PlayerIntentTest {
                     latch.countDown();
                 });
 
+        AtomicReference<QRCode> addedCodeAtomic = new AtomicReference<>();
+        playerDB.playerAddQRCode(mockCode1,
+                (addedCode, success) -> {
+                    successAtomic.set(success);
+                    addedCodeAtomic.set(addedCode);
+                    addLatch.countDown();
+                });
+
+        playerDB.playerAddQRCode(mockCode2,
+                (addedCode, success) -> {
+                    successAtomic.set(success);
+                    addedCodeAtomic.set(addedCode);
+                    addLatch.countDown();
+                });
+
+        playerDB.playerAddQRCode(mockCode3,
+                (addedCode, success) -> {
+                    successAtomic.set(success);
+                    addedCodeAtomic.set(addedCode);
+                    addLatch.countDown();
+                });
+
+
+
     }
 
     /**
@@ -107,29 +162,141 @@ public class PlayerIntentTest {
     @Test
     public void start() throws Exception{
         Activity activity = rule.getActivity();
+
+        removePlayer(mockPlayer);
+
     }
 
     /**
-     * Makes sure the player can open the camera to scan a QR Code for US 01.01.01
+     * Makes sure the player can open the camera to scan a QR Code for
+     * US 01.01.01
      */
     @Test
-    public void openCamera() throws InterruptedException{
+    public void openCamera() throws InterruptedException {
         solo.assertCurrentActivity("Wrong Activity", QuickNavActivity.class);
         solo.clickOnView(solo.getView(R.id.button_qn_scanCode));
         solo.assertCurrentActivity("Wrong Activity", CameraScanActivity.class);
-    }
 
-    /**
-     *
-     */
-    @Test
-    public void PLACEHOLDERNAMETEST() throws InterruptedException{
         removePlayer(mockPlayer);
     }
 
-    @After
+    /**
+     * Makes sure the player can access "My Library" and can click on a QR code to view it
+     * US 01.02.01
+     */
+    /*
+    @Test
+    public void viewLibrary() {
+
+        //Testing going to "My Library" and viewing all QR codes are in it: US 01.02.01
+        solo.assertCurrentActivity("Wrong Activity", QuickNavActivity.class);
+        solo.clickOnView(solo.getView(R.id.button_qn_cl));
+        solo.assertCurrentActivity("Wrong Activity", QRCodeLibraryActivity.class);
+        solo.clickOnText("500");
+        solo.assertCurrentActivity("Wrong Activity", QRCodeViewActivity.class);
+    }
+
+     */
+
+
+    /**
+     * Makes sure the player can use the REMOVE CODE button to delete a code from their account
+     * US 01.03.01
+     */
+    /*
+    @Test
+    public void removeQRCode() {
+        solo.assertCurrentActivity("Wrong Activity", QuickNavActivity.class);
+        solo.clickOnView(solo.getView(R.id.button_qn_cl));
+        solo.assertCurrentActivity("Wrong Activity", QRCodeLibraryActivity.class);
+        solo.clickOnText("400");
+        solo.assertCurrentActivity("Wrong Activity", QRCodeViewActivity.class);
+        solo.clickOnView(solo.getView(R.id.btn_remove_code));
+        solo.waitForText("Confirmation", 1, 2000);
+        solo.clickOnText("Yes");
+        solo.assertCurrentActivity("Wrong Activity", QRCodeLibraryActivity.class);
+    }
+
+     */
+
+
+    /**
+     * Navigates to "My Library" where they can sort their QR codes from highest to lowest and lowest to highest
+     * US 01.04.01
+     */
+    /*
+    @Test
+    public void highLowScore() {
+        solo.assertCurrentActivity("Wrong Activity", QuickNavActivity.class);
+        solo.clickOnView(solo.getView(R.id.button_qn_cl));
+        solo.assertCurrentActivity("Wrong Activity", QRCodeLibraryActivity.class);
+        solo.clickOnView(solo.getView(R.id.ml_sort_points_btn));
+        solo.searchText("500", 1);
+        solo.clickOnView(solo.getView(R.id.ml_sort_points_btn));
+        solo.searchText("100", 1);
+    }
+
+     */
+
+
+    /**
+     * Navigates to the players profile and checks for sum of their scores is kept as Total Points:
+     * US 01.05.01
+     */
+    @Test
+    public void totalScore() throws InterruptedException {
+        solo.assertCurrentActivity("Wrong Activity", QuickNavActivity.class);
+        solo.clickOnView(solo.getView(R.id.button_qn_profile));
+        solo.assertCurrentActivity("Wrong Activity", ProfileActivity.class);
+        solo.searchText("1000",1);
+
+        removePlayer(mockPlayer);
+    }
+
+
+
+    /**
+     * Navigates to the players profile and checks for the total codes a player has
+     * US 01.06.01
+     */
+    @Test
+    public void totalCodes() throws InterruptedException {
+
+        solo.enterText((EditText) solo.getView(R.id.edittext_signup_username), "John Doe");
+        solo.enterText((EditText) solo.getView(R.id.edittext_signup_email), "JDoe@noMail.com");
+        solo.clickOnView(solo.getView(R.id.button_signup_confirm));
+        solo.assertCurrentActivity("Wrong Activity", QuickNavActivity.class);
+        solo.clickOnView(solo.getView(R.id.button_qn_profile));
+        solo.assertCurrentActivity("Wrong Activity", ProfileActivity.class);
+        solo.searchText("3",1);
+
+        removePlayer(mockPlayer);
+    }
+
+
+    /**
+     * Navigates to the scoreboard and slicks on the first place player to ensure a user can view a player profile
+     * US 01.07.01
+     */
+/*
+    @Test
+    public void otherProfiles() {
+        solo.assertCurrentActivity("Wrong Activity", QuickNavActivity.class);
+        solo.clickOnView(solo.getView(R.id.button_qn_sb));
+        solo.assertCurrentActivity("Wrong Activity", ScoreboardActivity.class);
+        solo.clickOnText("1L!");
+        solo.assertCurrentActivity("Wrong Activity", ShowIndividualCodes.class);
+        solo.clickOnView(solo.getView(R.id.player_profile_btn));
+        solo.assertCurrentActivity("Wrong Activity", ShowUserProfile.class);
+    }
+
+ */
+
+
+
+        @After
     public void cleanUp() throws InterruptedException{
-    //    removePlayer(mockPlayer);
+        //removePlayer(mockPlayer);
     }
 
 }
