5ac3cbac68d1e4c2b7019846e596eb81f12a26cd
TannerLH <113555978+TannerLH@users.noreply.github.com>
2023-04-03 08:08:13 -0600

    Game QR Code Intent Tests

--- code/app/src/androidTest/java/com/example/ihuntwithjavalins/GameQRCodesIntentTest.java
+++ code/app/src/androidTest/java/com/example/ihuntwithjavalins/GameQRCodesIntentTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertTrue;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
@@ -12,6 +14,8 @@ import com.example.ihuntwithjavalins.Camera.CameraScanActivity;
 import com.example.ihuntwithjavalins.Player.Player;
 import com.example.ihuntwithjavalins.Player.PlayerDB;
 import com.example.ihuntwithjavalins.QRCode.QRCode;
+import com.example.ihuntwithjavalins.QRCode.QRCodeLibraryActivity;
+import com.example.ihuntwithjavalins.QRCode.QRCodeViewActivity;
 import com.example.ihuntwithjavalins.common.DBConnection;
 import com.robotium.solo.Solo;
 
@@ -23,6 +27,12 @@ import org.junit.Test;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicReference;
 
+/**
+ * PlayerIntentTest is a class that tests all the Player user stories (US 01.01.01 through US 01.07.01)
+ *
+ * @version 1.0
+ */
+
 public class GameQRCodesIntentTest {
 
     /**
@@ -32,9 +42,12 @@ public class GameQRCodesIntentTest {
     private static final long TIMEOUT = 5;
     PlayerDB playerDB;
     Player mockPlayer;
+    Player mockPlayer2;
+
     QRCode mockCode1;
     QRCode mockCode2;
     QRCode mockCode3;
+
 
 
     /**
@@ -61,6 +74,13 @@ public class GameQRCodesIntentTest {
         assertTrue(successAtomic.get());
     }
 
+    private void clearSharedData() {
+        Activity activity = rule.getActivity();
+        SharedPreferences mPrefs = activity.getSharedPreferences("Login", Context.MODE_PRIVATE);
+        mPrefs.edit().clear().commit();    // uncomment to easily clear the shared preferences for login testing
+        mPrefs.edit().putString("UsernameTag", "John Doe").apply();
+    }
+
 
     /**
      * Sets up the starting spot for all these tests at the QuickNavActivity
@@ -73,8 +93,14 @@ public class GameQRCodesIntentTest {
      * creates a player with 2 QR codes and logs in as that player
      * Creates an extra QR code for later testing
      */
+
     @Before
     public void setUp() throws Exception {
+
+
+        clearSharedData();
+        //Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();          //getTargetContext();
+        //context.getSharedPreferences("Login", 0).edit().clear().commit();
 
         CountDownLatch latch = new CountDownLatch(1);
         CountDownLatch addLatch = new CountDownLatch(1);
@@ -119,6 +145,8 @@ public class GameQRCodesIntentTest {
                     addLatch.countDown();
                 });
 
+
+
     }
 
     /**
@@ -127,6 +155,9 @@ public class GameQRCodesIntentTest {
     @Test
     public void start() throws Exception{
         Activity activity = rule.getActivity();
+
+        removePlayer(mockPlayer);
+
     }
 
     /**
@@ -134,17 +165,47 @@ public class GameQRCodesIntentTest {
      * US 02.01.01
      */
     @Test
-    public void openCamera() {
+    public void openCamera() throws InterruptedException {
         solo.assertCurrentActivity("Wrong Activity", QuickNavActivity.class);
         solo.clickOnView(solo.getView(R.id.button_qn_scanCode));
         solo.assertCurrentActivity("Wrong Activity", CameraScanActivity.class);
+
+        removePlayer(mockPlayer);
+    }
+
+    /**
+     * Makes sure the player can access "My Library" and can click on a QR code to view it
+     * US 01.02.01
+     */
+    @Test
+    public void viewLibrary() {
+
+        //Testing going to "My Library" and viewing all QR codes are in it: US 01.02.01
+        solo.assertCurrentActivity("Wrong Activity", QuickNavActivity.class);
+        solo.clickOnView(solo.getView(R.id.button_qn_cl));
+        solo.assertCurrentActivity("Wrong Activity", QRCodeLibraryActivity.class);
+        solo.clickOnText("500");
+        solo.assertCurrentActivity("Wrong Activity", QRCodeViewActivity.class);
     }
 
 
-
+    /**
+     * Makes sure the player can use the REMOVE CODE button to delete a code from their account
+     * US 02.02.01
+     */
+    @Test
+    public void comment() {
+        solo.assertCurrentActivity("Wrong Activity", QuickNavActivity.class);
+        solo.clickOnView(solo.getView(R.id.button_qn_cl));
+        solo.assertCurrentActivity("Wrong Activity", QRCodeLibraryActivity.class);
+        solo.clickOnText("400");
+        solo.assertCurrentActivity("Wrong Activity", QRCodeViewActivity.class);
+        solo.clickOnView(solo.getView(R.id.comment_btn));
+        solo.clickOnText("Confirm");
+    }
     @After
     public void cleanUp() throws InterruptedException{
-        removePlayer(mockPlayer);
+        //removePlayer(mockPlayer);
     }
 
 }
